d6cd74abf9e8e39568d2a744f63f95a5686065f6
exuberant-object-0311 <polly.c.reynolds@gmail.com>
2019-05-06 19:55:06 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,4 +1,9 @@
-function forLoop {
+function forLoop ( {
   for (let i = 0; i < 25; i++)
-  if (i = 1) 
+  if (i = 1) {
+    return "I am 1 strange loop"
+  }
+  else if (i > 1) {
+    return "I am ${i} strange loop"
+  } )
 }
