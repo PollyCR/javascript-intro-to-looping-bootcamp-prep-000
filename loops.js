7d281ec7bc94ccad7c7d8f87285ee48fa03e3431
exuberant-object-0311 <polly.c.reynolds@gmail.com>
2019-05-06 20:20:42 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,17 +1,3 @@
-function makeArray() {
-  const array = []
-  const t = 25
-
-  for (let i = 1; i <2; i++) {
-    console.log("I am a strange loop.")
-  }
-
-  return [array, t]
+function forLoop {
+  
 }
-
-     const [array, t] = makeArray()
-      const strangeArray = forLoop(array)
-      const testArray = strangeArray.slice(array.length)
-
-      let first = "I am 1 strange loop."
-      let rest = "I am 24 strange loops."
