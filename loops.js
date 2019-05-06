28d029243970aca0da1e1a6956d2ec51fd4ef464
exuberant-object-0311 <polly.c.reynolds@gmail.com>
2019-05-06 19:59:26 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,11 +1,12 @@
 var array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
 
-function forLoop ( {
-  for (let i = 0; i =< 25; i++)
-  if (i = 1) {
-    return "I am 1 strange loop"
-  }
-  else if (i > 1) {
-    return "I am ${i} strange loop"
-  } )
+function forLoop (array) {
+ for (var i = 1; i < 25; i++) {
+   (let i = 1) {
+     console.log("I am ${i} strange loop")
+ }
+(let i < 25) {
+   console.log("I am ${i} strange loops")
+ }
+}
 }
